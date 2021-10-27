a072f7cc0884e4135e9ac8c1fe1202b01c46fd8b
mastercodercat <mastercoderoy64@gmail.com>
2021-10-27 10:44:13 -0700

    fixed: authentication model

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { users } from "@prisma/client";
+import { v2_users } from "@prisma/client";
 
 import db from "../db";
 import config from "../config";
 import { comparePasswords } from "../utils/password";
 
-const tokenForUser = (user: users) => {
+const tokenForUser = (user: v2_users) => {
   const timestamp = new Date().getTime();
   return jwt.sign({ sub: user.id, iat: timestamp }, config.appSecret);
 };
@@ -15,7 +15,7 @@ export const signIn = async (req: express.Request, res: express.Response) => {
   const { email, password } = req.body;
 
   try {
-    const user = await db.users.findFirst({
+    const user = await db.v2_users.findFirst({
       where: {
         email: {
           equals: email,
@@ -27,11 +27,14 @@ export const signIn = async (req: express.Request, res: express.Response) => {
       return res.status(401).json({ error: "Incorrect Credentials" });
     }
 
-    const isMatch = await comparePasswords(password, user.password);
+    const isMatch = await comparePasswords(password, user.password as string);
+
     if (isMatch) {
+      const token = tokenForUser(user);
+
       return res.send({
-        token: tokenForUser(user),
-        user: user,
+        token,
+        user,
       });
     } else {
       return res.status(401).json({ error: "Incorrect Credentials" });
@@ -50,7 +53,7 @@ export const signUp = async (req: express.Request, res: express.Response) => {
       .send({ error: "Username, email and password must be provided" });
   }
 
-  const existingUser = await db.users.findFirst({
+  const existingUser = await db.v2_users.findFirst({
     where: {
       email: email,
     },
@@ -60,9 +63,9 @@ export const signUp = async (req: express.Request, res: express.Response) => {
     return res.status(422).send({ error: "Email is aleready in use..." });
   }
 
-  const user = await db.users.create({
+  const user = await db.v2_users.create({
     data: {
-      name,
+      username: name,
       email,
       password,
     },
@@ -81,7 +84,7 @@ export const resetPassword = async (
 ) => {
   const { email } = req.body;
 
-  const user = await db.users.findFirst({
+  const user = await db.v2_users.findFirst({
     where: {
       email: email,
     },
@@ -91,7 +94,7 @@ export const resetPassword = async (
     return res.status(401).json({ error: "Incorrect Credentials " });
   }
 
-  await db.users.update({
+  await db.v2_users.update({
     where: { id: user.id },
     data: { password: "password" },
   });
